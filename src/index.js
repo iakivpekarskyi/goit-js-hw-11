9cc8bfcd73129ff78c6f7355dd5c03b0eccfcbd0
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-01-19 17:22:07 +0200

    finished fetch func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchPictures from './js/fetchPictures';
+
+function createGalleryMarkup(fetchPictures) {
+  return fetchPictures
+    .map(({ original, preview, description }) => {
+      return `
+<div class="gallery">
+  <a href="${original}">
+    <img src="${preview}" alt="${description}" />
+  </a>
+</div>
+      `;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', gallery);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/fetchPictures.js
+++ src/js/fetchPictures.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
 const API_KEY = '32920278-ac1f185981fb853c8559f3f77';
-const filters = `?key=${API_KEY}&q=${value}&image_type=photo&orientation=horozontal&safesearch=true&per_page=40`;
+const filters = `?key=${API_KEY}&q={value}&image_type=photo&orientation=horozontal&safesearch=true&per_page=40`;
 
-export async function fetchPictures(value) {
+export async function fetchPictures(inputValue) {
   try {
-    return await axios.get(`${BASE_URL}${filters}`);
+    const data = await axios.get(`${BASE_URL}${filters}`);
+    return data;
+    console.log(data);
   } catch (err) {
     console.log(err);
   }
