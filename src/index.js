40efecf85a922c157b37a511bc985a78b593180c
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-01-19 22:22:41 +0200

    installed handlebars - haven't added yet

--- src/index.js
+++ src/index.js
@@ -3,22 +3,40 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchPictures from './js/fetchPictures';
 
-function createGalleryMarkup(fetchPictures) {
-  return fetchPictures
-    .map(({ original, preview, description }) => {
-      return `
-<div class="gallery">
-  <a href="${original}">
-    <img src="${preview}" alt="${description}" />
-  </a>
-</div>
-      `;
-    })
-    .join('');
+const searchInput = document.querySelector('.search-form__input');
+const searchBtn = document.querySelector('.search-form__button');
+
+searchInput.addEventListener('input', onSearch);
+
+function onSearch() {
+  const inputValue = searchInput.value.trim();
+  if (!inputValue) {
+    return;
+  }
 }
 
-galleryContainer.insertAdjacentHTML('beforeend', gallery);
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionDelay: 250,
-  captionsData: 'alt',
+fetchPictures(searchInput).then(pictures => {
+  Notify.info('Hooray! We found ${totalHits} images.');
+  return;
 });
+
+//   function createGalleryMarkup(fetchPictures) {
+//     return fetchPictures
+//       .map(({ original, preview, description }) => {
+//         return `
+// <div class="gallery">
+//   <a href="${original}">
+//     <img src="${preview}" alt="${description}" />
+//   </a>
+// </div>
+//       `;
+//       })
+//       .join('');
+//   }
+
+//   galleryContainer.insertAdjacentHTML('beforeend', gallery);
+//   const lightbox = new SimpleLightbox('.gallery a', {
+//     captionDelay: 250,
+//     captionsData: 'alt',
+//   });
+//
