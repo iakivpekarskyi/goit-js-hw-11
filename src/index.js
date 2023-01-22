c18b8461d810b741a2d42974e4da4647836a58de
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-01-22 02:34:25 +0200

    finished gallery markup

--- src/index.js
+++ src/index.js
@@ -4,29 +4,66 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchInput = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
 
-// const gallery = document.querySelector('.gallery');
+const SimpleLightboxGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 searchInput.addEventListener('submit', onSearch);
 
 function onSearch(event) {
   event.preventDefault();
-  const inputValue = event.currentTarget.searchQuery.value;
-  if (!inputValue !== '') {
-    fetchImages();
+  gallery.innerHTML = '';
+  const inputValue = event.currentTarget.searchQuery.value.trim();
+  console.log(event.currentTarget.searchQuery.value);
+  if (!inputValue) {
     return;
   }
-}
-
-fetchImages(inputValue).then(response => {
-  const result = response.data.hits;
-  console.log(result);
-  if (result === 0) {
-    Notify.warning(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  } else {
-    // galleryMarkup(data.hits);
-    Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
+  fetchImages(inputValue).then(response => {
+    const result = response.data.hits.length;
+    if (result === 0) {
+      gallery.innerHTML = '';
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      gallery.innerHTML = '';
+      galleryMarkup(response.data.hits);
+      Notify.info(`Hooray! We found ${response.data.totalHits} images.`);
+    }
+  });
+  function galleryMarkup(images) {
+    console.log(images);
+    const markup = images
+      .map(
+        image =>
+          `
+<div class="photo-card">
+<ion-icon name="cloud-download-outline"></ion-icon>
+ <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views:${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments:${image.comments}</b>
+    </p>
+    <p class="info-item">
+    <ion-icon name="cloud-download-outline"></ion-icon>
+      <b>Downloads:${image.downloads}</b>
+    </p>
+  </div>
+</div>
+`
+      )
+      .join('');
+    gallery.innerHTML = markup;
+    SimpleLightboxGallery.refresh();
   }
-});
+}
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -4,6 +4,7 @@ export const fetchImages = async inputValue => {
   const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '32920278-ac1f185981fb853c8559f3f77';
   const filter = `?key=${API_KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`;
+
   return await axios.get(`${BASE_URL}${filter}`);
 };
 
